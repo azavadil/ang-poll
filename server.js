b3454d78a1eac92c96d4b28d62f03b76c61ce429
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-08-18 14:18:11 -0700

    build server

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-var http = require("http");
-var handler = require("./request-handler");
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('bower_components'));
+app.use(express.static('app'));
 
 var port = process.env.PORT || 8080;
 var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer();
 
-server.on("request", function(req, res){
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(process.cwd(), uri);
 
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      utils.sendResponse(res);
-      return;
-    } else if (uri === '/') {
-      filename += 'app/index.html';
-    }
 
-    var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-    res.writeHead(200, mimeType);
 
-    var fileStream = fs.createReadStream(filename);
-    fileStream.pipe(res);
 
-  });
-};
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/app/index.html');
+});
 
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+app.listen(port, ip);
