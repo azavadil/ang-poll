79ede1344b7e9977794dff73dc0dd94174117953
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-08-27 07:46:19 -0700

    Change server for testing

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ var app = express();
 
 
 
-app.use(express.static(__dirname + '/app'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+// app.use(express.static(__dirname + '/app'));
+// app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-var port = process.env.PORT || 8080;
-var ip = process.env.IP || "127.0.0.1";
-
-
+// var port = process.env.PORT || 8080;
+// var ip = process.env.IP || "127.0.0.1";
 
 
+
+
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/app/index.html');
+// });
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/app/index.html');
-});
+  res.send('hello world');
+})
 
 console.log("Listening on http://" + ip + ":" + port);
 app.listen(port, ip);
