1c9bf4cbca8aefafdb411eabe35b181a41da983d
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-08-27 07:58:28 -0700

    Change back to server.js

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ var app = express();
 
 
 
-// app.use(express.static(__dirname + '/app'));
-// app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 var port = process.env.PORT || 8080;
 var ip = process.env.IP || "127.0.0.1";
 
 
 
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/app/index.html');
+});
 
 // app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/app/index.html');
-// });
-
-app.get('/', function(req, res){
-  res.send('hello world');
-})
+//   res.send('hello world');
+// })
 
 console.log("Listening on http://" + ip + ":" + port);
 app.listen(port, ip);
